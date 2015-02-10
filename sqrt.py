b4a9215d91173b487c956f83c2aa513ae5a4d4b7
Sean <extendvg@gmail.com>
2015-02-10 18:54:09 +0800

    delete file

--- sqrt.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from match import sqrt
-for x in range(99,0,-1):
-	root = sqrt(n)
-	if root == int(root):
-		print n
-		break
--- walk.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding=utf-8 -*-
-import os
-sep = os.sep
-for root,dir,file in os.walk('/data/soft'):
-	for i in file:
-		a = (root+sep+i)
-		print a
