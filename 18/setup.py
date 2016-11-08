dc8a2ba23f89bae5559903c985c5c5f1371cdcbf
Sean Ren <extendvg@gmail.com>
2016-11-08 16:38:11 +0800

    delete empty file

--- 18/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding=utf-8 -*-
-from distutils.core import setup
-
-setup(name='hello',
-        version='1.0',
-        description='say hello',
-        py_modules=['hello']
-        )
--- cherry/complex_site.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding=utf-8 -*-
-import cherrypy
-import os,sys
-
-class homepage(object):
-    def __init__(self):
-        a
